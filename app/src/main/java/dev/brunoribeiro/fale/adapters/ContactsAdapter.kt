dc106c0e60e54acc0f285c36465c86323e1c194b
Bruno Ribeiro <brunorbds50@gmail.com>
2021-02-18 17:12:04 -0300

    Adding data binding to recyclerview

--- app/src/main/java/dev/brunoribeiro/fale/adapters/ContactsAdapter.kt
+++ app/src/main/java/dev/brunoribeiro/fale/adapters/ContactsAdapter.kt
@@ -8,32 +8,40 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import dev.brunoribeiro.fale.R
+import dev.brunoribeiro.fale.databinding.ItemContactBinding
 import dev.brunoribeiro.fale.entities.Contact
 
 class ContactsAdapter(private val listsOfContacts: List<Contact>): RecyclerView.Adapter<ContactsAdapter.ContactViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactViewHolder {
-        val itemview = LayoutInflater.from(parent.context).inflate(R.layout.item_contact, parent, false)
-
-        return ContactViewHolder(itemview)
+        return  ContactViewHolder.from(parent)
     }
 
     override fun onBindViewHolder(holder: ContactViewHolder, position: Int) {
-        val current = listsOfContacts[position]
-
-        holder.nameContact.text = current.name
-        holder.lastTextContact.text = current.messages[listsOfContacts.size - 1].text
-        holder.dateContact.text = current.messages[listsOfContacts.size - 1].date
-        holder.imgContact.setImageResource(R.drawable.person1)
-
+        val item = listsOfContacts[position]
+        holder.bind(item)
     }
 
     override fun getItemCount(): Int  = listsOfContacts.size
 
 
-    class ContactViewHolder(view: View): RecyclerView.ViewHolder(view){
-        val nameContact = view.findViewById<TextView>(R.id.name_contact)
-        val lastTextContact = view.findViewById<TextView>(R.id.text_contact)
-        val imgContact = view.findViewById<ImageView>(R.id.img_contact)
-        val dateContact = view.findViewById<TextView>(R.id.date_contact)
+    class ContactViewHolder(val binding: ItemContactBinding): RecyclerView.ViewHolder(binding.root){
+
+        fun bind(item: Contact){
+            with(binding){
+                nameContact.text = item.name
+                textContact.text = item.messages[0].text
+                dateContact.text = item.messages[0].date
+                imgContact.setImageResource(R.drawable.person1)
+            }
+        }
+
+
+        companion object {
+            fun from(parent: ViewGroup): ContactViewHolder {
+                val inflater = LayoutInflater.from(parent.context)
+                val binding = ItemContactBinding.inflate(inflater, parent, false)
+                return ContactViewHolder(binding)
+            }
+        }
     }
 }
